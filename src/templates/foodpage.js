0b7b460996592645952b90e4a0589b30c618f9d3
pahosler <pahosler@gmail.com>
2019-08-12 15:42:44 -0500

    stuffing

--- src/templates/foodpage.js
+++ src/templates/foodpage.js
@@ -4,41 +4,28 @@ import Layout from '../components/Layout'
 
 import { graphql } from 'gatsby'
 
-export const FoodPageTemplate = ({
-  menu
-}) => (
+export const FoodPageTemplate = ({ menu }) => (
   <main
     style={{
-      backgroundImage: `url('../img/main_background.png') no-repeat center center fixed`
-    }}>
-    <div
-    style={{
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      height: '100vh',
-      width: '100vw',
-      backgroundColor: 'black',
-      zIndex: -10
+      backgroundImage: `url('../img/main_background.png') no-repeat center center fixed`,
     }}>
     <div
       style={{
         position: 'fixed',
-        height: '100%',
-        width: '100%',
-        backgroundImage: `url("../img/main_background.png")`,
-        backgroundRepeat: 'none',
-        backgroundSize: 'cover',
-        zIndex: -5
-      }}
-    />
+        top: 0,
+        left: 0,
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: 'black',
+        zIndex: -10,
+      }}>
+      <embed src={menu} title="menu" style={{ width: '100%', height: 1500 }} />
     </div>
-    <embed src={menu} title="menu" style={{width:"100%", height:1500}} />  
   </main>
 )
 
 FoodPageTemplate.propTypes = {
-  menu: PropTypes.string
+  menu: PropTypes.string,
 }
 
 const FoodPage = ({ data }) => {
@@ -46,9 +33,7 @@ const FoodPage = ({ data }) => {
 
   return (
     <Layout>
-      <FoodPageTemplate
-        menu={frontmatter.menu.publicURL}
-      />
+      <FoodPageTemplate menu={frontmatter.menu} />
     </Layout>
   )
 }
@@ -67,9 +52,7 @@ export const pageQuery = graphql`
   query FoodPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "foodpage" } }) {
       frontmatter {
-        menu {
-          publicURL
-        }
+        menu
       }
     }
   }
