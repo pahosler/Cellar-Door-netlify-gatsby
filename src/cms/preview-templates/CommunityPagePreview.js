13d313bc661da2748400f0d5296e0f5960477e53
pahosler <pahosler@gmail.com>
2019-12-11 15:44:19 -0600

    edit community preview-4

--- src/cms/preview-templates/CommunityPagePreview.js
+++ src/cms/preview-templates/CommunityPagePreview.js
@@ -5,8 +5,8 @@ import { CommunityPageTemplate } from '../../templates/community-page'
 const CommunityPagePreview = ({ entry, getAsset }) => {
   const data = entry.getIn(['data']).toJS()
 
-//  const entryImages = entry.getIn(['data', 'gallery', 'images'])
-//  const images = entryImages ? entryImages.toJS() : []
+  const entryImages = entry.getIn(['data', 'gallery'])
+  const images = entryImages ? entryImages.toJS() : []
 
   if (data) {
     return (
@@ -14,7 +14,7 @@ const CommunityPagePreview = ({ entry, getAsset }) => {
         background_image={data.background_image}
         title={data.title}
         menu={data.menu}
-        gallery={ data.gallery.images }
+        gallery={{ images }}
       />
     )
   } else {
